480b107c8116de26a665e8b4e5f86b26801ee01d
Mifkiyan <m.favian199@gmail.com>
2023-04-30 21:15:28 +0700

    feat: improve date and calculator feature

--- src/algorithm/getAnswer.js
+++ src/algorithm/getAnswer.js
@@ -5,8 +5,9 @@ const { get } = require("mongoose");
 
 // ini fungsi utamanya, harusnya regex di sini buat nentuin question fitur apa
 export function getAnswer(question) {
-  const dateRegex = /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/;
-  const mathRegex = /^[\d\s\+\-\*\/\(\)]+$/;
+  const dateRegex = /^.*(\d{1,2})\/(\d{1,2})\/(\d{4}).*$/;
+  // math regex with whitespace
+  const mathRegex = /^.*(\d+)(\s*)(\+|\-|\*|\/)(\s*)(\d+).*$/;
 
   // Disini harusnya pake algoritma KMP sama BM nya (kayaknya)
   const isDate = dateRegex.test(question);
@@ -26,10 +27,19 @@ export function getAnswer(question) {
   // dst
 }
 
-export function calculator(expression) {
+export function calculator(question) {
   try {
+    // if theres a number followed by a space and another number, throw an error
+    if (/[0-9]\s+[0-9]/.test(question)) {
+      throw new Error('Sintaks persamaan tidak sesuai');
+    }
+
     // Remove any whitespace from the expression
-    expression = expression.replace(/\s/g, '');
+    question = question.replace(/\s/g, '');
+
+    // cut the expression to only contain numbers and operators
+    const expressionRegex = /[0-9()+\-*/].*[0-9()+\-*/]/;
+    const expression = question.match(expressionRegex)[0];
 
     // Initialize the stack for numbers and operators
     const numStack = [];
@@ -108,15 +118,15 @@ export function calculator(expression) {
 
     // The final result will be the only element left in the number stack
     if (numStack.length !== 1) {
-      throw new Error('Invalid expression');
+      throw new Error('Sintaks persamaan tidak sesuai');
     }
 
     const result = numStack[0];
     if (isNaN(result)) {
-      throw new Error('Invalid expression');
+      throw new Error('Sintaks persamaan tidak sesuai');
     }
 
-    return result.toString();
+    return "Hasilnya adalah " + result.toString();
   } catch (error) {
     return error.message;
   }
@@ -124,7 +134,7 @@ export function calculator(expression) {
 
 export function date(question) {
   try {
-    const dateRegex = /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/;
+    const dateRegex = /(\d{1,2})\/(\d{1,2})\/(\d{4})/;
     const match = question.match(dateRegex);
 
     const day = parseInt(match[1]);
@@ -134,7 +144,7 @@ export function date(question) {
     const date = new Date(year, month - 1, day);
 
     if (date.getDate() !== day || date.getMonth() !== month - 1 || date.getFullYear() !== year) {
-      throw new Error('Invalid date');
+      throw new Error('Masukan tanggal tidak sesuai');
     }
 
     const dayOfWeek = date.getDay();
@@ -147,14 +157,15 @@ export function date(question) {
   }
 }
 
-
-console.log(getAnswer('(5*8)+3+s'));
-console.log(getAnswer('(3+2)*(x(8-5)*2)'));
-console.log(getAnswer('3 * (2 + 5) - 4 / (1 + 1)'));
-
-console.log(getAnswer('30/04/2023'));
-console.log(getAnswer('31/4/2021')); // April gaada tanggal 31
-console.log(getAnswer('30/x2/2021')); // Masuknya jadi teks
+// console.log(getAnswer('Apa ibukota Indonesia?'));
+// console.log(getAnswer('Apa mata kuliah IF semester 4 yang paling keos?'));
+// console.log(getAnswer('Apa ibukota Indonsa'));
+// console.log(getAnswer('Apa ibukota la'));
+// console.log(getAnswer('Coba hitung 1 * ( 2 + 29 )'));
+// console.log(getAnswer('5+2*5+?'));
+// console.log(getAnswer('Hari apa 25/02/2023'));
+// console.log(getAnswer('31/01/2050'));
+// console.log(getAnswer('31/02/2050'));
 
 // const data = getQna();
 // console.log(data);
