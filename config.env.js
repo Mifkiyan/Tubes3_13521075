e0e44f5cf1b1021de5dd5915b36d22567002fef6
Febryan Arota <13521120@mahasiswa.itb.ac.id>
2023-05-04 16:11:27 +0700

    fix deploy

--- config.env.js
+++ config.env.js
@@ -1,4 +1,4 @@
 export default {
   URL : "mongodb+srv://13521120:13521120pw@cluster0.dmexdas.mongodb.net/?retryWrites=true&w=majority",
-  BASE_URL : "http://localhost:3000/api/"
+  BASE_URL : "https://tubes3-13521075.vercel.app/"
 }
